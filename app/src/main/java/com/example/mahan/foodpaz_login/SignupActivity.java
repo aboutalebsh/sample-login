baadc3ef1b82e2deb85c4c0794877429f0b25b10
mahan <aboutaleb.shirangi@gmail.com>
2017-10-11 13:56:09 +0330

    add login to github

---
+++ app/src/main/java/com/example/mahan/foodpaz_login/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.mahan.foodpaz_login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
